14eeaf920102b2eb01d4cee193eb8ac5d439b91b
Hudson <hud.lucasgreg@gmail.com>
2026-04-01 21:40:55 -0300

    Atividade

--- index.js
+++ index.js
@@ -1,61 +1,65 @@
 const express = require("express");
 
 const app = express();
-
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-/*app.get("/", function (req, res) {
-  res.send("<h1>bem vindo ao meu primeiro site em node.js</h1>");
-});
-
- app.get("/2", function (req, res) {
-  res.send("<h1>bem vindo a segunda pagina</h1>");
-}); */
-
+// HOME
 app.get("/", function (req, res) {
+  const mapa = {img: "img/Mapa1.webp"}
   res.render("home");
 });
 
-app.get("/clientes", function (req, res) {
-  const listaClientes = ['jorge', 'douglas', 'luiz', 'demiurgo']
-  res.render("clientes", {
-    listaClientes : listaClientes
+// PERSONAGENS
+app.get("/personagens", function (req, res) {
+  const personagens = [
+    { nome: "Yumei", classe: "Euclid", descricao: "a aparência de uma jovem humana do sexo feminino, estimada entre 18 e 22 anos, com traços asiáticos comuns. O cabelo da entidade é naturalmente dividido em duas cores distintas: preto e branco, sem evidência de pigmentação artificia", img: "img/Yumei.webp"},
+    { nome: "Doren", classe: "Euclid", descricao: "SCP-1618 é um indivíduo humanoide do sexo masculino, de aparência ███, identificado como ██████. A idade aparente do objeto é estimada em ███ anos, embora exames fisiológicos indiquem inconsistências que impedem confirmação precisa.", img:"img/doren.webp" },
+    { nome: "Mizuichi", classe: "Keter", descricao: "uma entidade humanoide de constituição esguia que assume a aparência de um homem adulto Características visuais notáveis incluem: Cabelos longos, segmentados em múltiplas estruturas semelhantes a tentáculos. Olhos permanentemente obscurecidos pelos fios capilares. Presença de um organismo cefalóide aderido à região craniana, descrito como uma água-viva de coloração preta e vermelha, dotada de dentição e apêndices móveis.", img:"img/mizuichi.webp" }
+  ];
+
+  res.render("personagens", {
+    personagens: personagens
   });
-})
+});
 
-app.get("/clientes/:clientes", function (req, res) {
-  const clientes = req.params.clientes;
-  res.render("detalhesClientes", {
-    clientes:clientes,
+// DETALHE DO PERSONAGEM
+app.get("/personagens/:nome", function (req, res) {
+  const nome = req.params.nome;
+
+  res.render("detalhesPersonagem", {
+    nome: nome
   });
-})
+});
 
-app.get("/Produtos", function (req, res) {
-  const listaProdutos = ['celular', 'computador', 'tablet', 'demiurgo']
-  res.render("Produtos", {
-    listaProdutos : listaProdutos
+// REGRAS
+app.get("/regras", function (req, res) {
+  const regras = [
+    "Sistema baseado em 1d20 + atributo",
+    "Vitalidade = 10 + Vigor",
+    "Testes de imunidade evitam mutação"
+  ];
+
+  res.render("regras", {
+    regras: regras
   });
-})
+});
 
-app.get("/Produtos/:produto", function (req, res) {
-  const produto = req.params.produto;
-  res.render("detalhesProduto", {
-    produto:produto,
+// HISTÓRIA
+app.get("/historia", function (req, res) {
+
+  const historia = "A historia do mundo";
+
+  res.render("historia", {
+    historia: historia
   });
-})
-
-app.get("/Servicos", function (req, res) {
-  res.render("Servicos");
-})
-
+});
 
 const port = 8080;
 app.listen(port, (error) => {
   if (error) {
     console.log(`ocorreu um erro ao iniciar o servidor! ${error}`);
   } else {
-    console.log(
-      `Servidor iniciado com sucesso no endereço http://localhost:${port}`,
-    );
+    console.log(`Servidor iniciado com sucesso em http://localhost:${port}`);
   }
 });
